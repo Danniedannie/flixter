c350492a6aa9166ef605146fc76537c33b4873ee
danniedanniedannie <danielle.simone.lynch@gmail.com>
2019-06-11 12:08:00 +0000

    troubleshooting heroku with controller edits

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,6 +1,6 @@
 class Instructor::CoursesController < ApplicationController
 	before_action :authenticate_user!
-	before_action :require_authorized_for_current_course, only:[:show]
+	  before_action :require_authorized_for_current_course, only: [:show]
 
 def new 
 	@course = Course.new 
@@ -20,12 +20,11 @@ def show
 end
 
 private 
-
-def require_authorized_for_current_course
-	if current_course.user != current_user 
-		render plain: "Unauthorized", status :unauthorized
-	end 
-end 
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
+    end
+  end
 
 helper_method :current_course
 

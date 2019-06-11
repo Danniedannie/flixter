87c0bff48dd454070b5190d9a17dd225788429ad
danniedanniedannie <danielle.simone.lynch@gmail.com>
2019-06-11 12:23:27 +0000

    editing controller to troubleshoot heroku

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -16,11 +16,14 @@ end
 	def require_authorized_for_current_course
 		if current_course.user != current_user 
 			render plain: "Unauthorized", status: :unauthorized 
+		end 
+	end 
 
 	helper_method :current_course 
 	def :current_course 
 		@current_course ||= course.find(params[:course_id])
 		end 
+
 
 	def section_params 
 		params.require(:section).permit(:title)

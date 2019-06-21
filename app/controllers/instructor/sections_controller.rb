71aecce20f613399543590b48bd1fbe3a552a3b5
danniedanniedannie <danielle.simone.lynch@gmail.com>
2019-06-21 12:17:35 +0000

    editing styles on the course instructor page

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,6 @@
 class Instructor::SectionsController < ApplicationController
 before_action  :authenticate_user!
-before_action :require_authorized_for_current_course
+before_action :require_authorized_for_current_course 
 
 def new
 	@section = Section.new 
@@ -11,7 +11,20 @@ def create
     redirect_to instructor_course_path(current_course)
   end
 
+  def update 
+  	 current_course.update_attributes(lesson_params)
+  	 render plain: 'updated!'
+
+  end 
+
 	private 
+
+	  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      render plain: 'Unauthorized', status: :unauthorized
+    end
+  end
+
 
 	def require_authorized_for_current_course
 		if current_course.user != current_user 
@@ -19,13 +32,17 @@ def create
 		end 
 	end 
   
-  helper_method :current_course
-  def current_course
+helper_method :current_course
+def current_course
+  if params[:course_id]
     @current_course ||= Course.find(params[:course_id])
+  else
+    current_section.course
   end
+end
 
 	def section_params 
-		params.require(:section).permit(:title)
+		params.require(:section).permit(:title, :row_order_position)
 	end 
 
 end
